9a3c39707ea4bebd1b3e109039c66801a06d7f0e
Jordan <uberbomb@yahoo.com>
2022-08-28 11:56:25 -0400

    Add module to create and control display
    Added displayController module to control the display. Added functions
    to remove title screen, set game mode, and create gameboard. Added
    styling to gameboard.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 const gameBoard = (function() {
   board = ["", "", "",
           "", "", "",
@@ -50,6 +51,84 @@ const gameBoard = (function() {
   return {playerMove}
 })();
 
+const displayController = (function() {
+  const createTitle = () => {
+    const titleScreenDiv = document.createElement('div');
+    const pWelcome = document.createElement('p');
+    const pTitle = document.createElement('p');
+    const pSelect = document.createElement('p');
+    const selectBtnsDiv = document.createElement('div');
+
+    pWelcome.innerText = "Welcome to";
+    pTitle.innerText = "Tic-Tac-Toe";
+    pSelect.innerText = "Select your game mode below";
+  
+    titleScreenDiv.setAttribute('id', 'title-screen');
+    document.body.append(titleScreenDiv);
+    titleScreenDiv.append(pWelcome, pTitle, pSelect, selectBtnsDiv);
+  
+    const twoPlayerBtn = document.createElement('button');
+    const vsComputerBtn = document.createElement('button');
+
+    selectBtnsDiv.setAttribute('id', 'select-btns');
+    twoPlayerBtn.setAttribute('id', 'two-player-btn')
+;
+    twoPlayerBtn.innerText = "2 Player";
+    vsComputerBtn.innerHTML = "vs Computer";
+
+    selectBtnsDiv.append(twoPlayerBtn, vsComputerBtn);
+
+    twoPlayerBtn.setAttribute('id', 'two-player-btn');
+    twoPlayerBtn.addEventListener('click', () => {
+      titleScreenDiv.remove();
+      createBoard();
+    });
+  
+    vsComputerBtn.setAttribute('id', 'vs-computer-btn');
+    vsComputerBtn.addEventListener('click', () => {
+      console.log("This feature has not been added yet");
+    });
+  }
+
+  const createBoard = () => {
+    console.log('hi');
+    const boardContainerDiv = document.createElement('div');
+    boardContainerDiv.setAttribute('id', 'board-container');
+    document.body.append(boardContainerDiv);
+    for(let i = 0; i < 9; i++){
+      console.log(i);
+      const gridDiv = document.createElement('div');
+      gridDiv.setAttribute('id', i);
+      gridDiv.classList.add('test');
+      boardContainerDiv.append(gridDiv);
+    }
+
+    const test = document.querySelectorAll('.test');
+    let counter = 0;
+    test.forEach((item) => {
+      item.addEventListener('click', () => {
+        if (counter % 2 == 0) {
+          if (!item.hasChildNodes()){
+            item.textContent = "O";
+            item.style.color = "rgba(66, 179, 255, 0.5)";
+            gameBoard.playerMove("O", item.id);
+            counter++;
+          }
+        } else if (counter % 2 == 1) {
+          if (!item.hasChildNodes()){
+            item.textContent = "X";
+            item.style.color = "rgb(255, 102, 102)";
+            gameBoard.playerMove("X", item.id);
+            counter++;
+          }
+        }
+      });
+    });
+    
+  }
+  return {createTitle}
+})();
+
 const test = document.querySelectorAll('.test');
 let counter = 0;
 test.forEach((item) => {
@@ -57,17 +136,19 @@ test.forEach((item) => {
     if (counter % 2 == 0) {
       if (!item.hasChildNodes()){
         item.textContent = "O";
-        item.style.color = "blue";
+        item.style.color = "rgba(66, 179, 255, 0.5)";
         gameBoard.playerMove("O", item.id);
         counter++;
       }
     } else if (counter % 2 == 1) {
       if (!item.hasChildNodes()){
         item.textContent = "X";
-        item.style.color = "red";
+        item.style.color = "rgb(255, 102, 102)";
         gameBoard.playerMove("X", item.id);
         counter++;
       }
     }
   });
 });
+
+displayController.createTitle();
