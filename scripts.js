3b82d2074d0e6ff1870c942afcf2a0df95a21465
Jordan <uberbomb@yahoo.com>
2022-08-28 17:32:51 -0400

    Add functions to reset board and return to title screen
    Added functions to reset gameboard, return to title screen, and adjusted
    styling.

--- scripts.js
+++ scripts.js
@@ -11,47 +11,48 @@ const gameBoard = (function() {
   
   const winCheckX = () => {
     if(board[0] === "X" && board[1] === "X" && board[2] === "X") {
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[3] === "X" && board[4] === "X" && board[5] === "X"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[6] === "X" && board[7] === "X" && board[8] === "X"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[0] === "X" && board[4] === "X" && board[8] === "X"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[2] === "X" && board[4] === "X" && board[6] === "X"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[0] === "X" && board[3] === "X" && board[6] === "X"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[1] === "X" && board[4] === "X" && board[7] === "X"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[2] === "X" && board[5] === "X" && board[8] === "X"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     }
   }
 
   const winCheckO = () => {
     if(board[0] === "O" && board[1] === "O" && board[2] === "O") {
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[3] === "O" && board[4] === "O" && board[5] === "O"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[6] === "O" && board[7] === "O" && board[8] === "O"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[0] === "O" && board[4] === "O" && board[8] === "O"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[2] === "O" && board[4] === "O" && board[6] === "O"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[0] === "O" && board[3] === "O" && board[6] === "O"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[1] === "O" && board[4] === "O" && board[7] === "O"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     } else if(board[2] === "O" && board[5] === "O" && board[8] === "O"){
-      console.log("Winner Winner Chicken Dinner!");
+      alert("Winner Winner Chicken Dinner!");
     }
   }
-  return {playerMove}
+  return {playerMove, board}
 })();
 
 const displayController = (function() {
+
   const createTitle = () => {
     const titleScreenDiv = document.createElement('div');
     const pWelcome = document.createElement('p');
@@ -71,37 +72,61 @@ const displayController = (function() {
     const vsComputerBtn = document.createElement('button');
 
     selectBtnsDiv.setAttribute('id', 'select-btns');
-    twoPlayerBtn.setAttribute('id', 'two-player-btn')
-;
+    twoPlayerBtn.setAttribute('id', 'two-player-btn');
+    twoPlayerBtn.classList.add('btn');
+    vsComputerBtn.setAttribute('id', 'vs-computer-btn');
+    vsComputerBtn.classList.add('btn');
+
     twoPlayerBtn.innerText = "2 Player";
     vsComputerBtn.innerHTML = "vs Computer";
 
     selectBtnsDiv.append(twoPlayerBtn, vsComputerBtn);
 
-    twoPlayerBtn.setAttribute('id', 'two-player-btn');
     twoPlayerBtn.addEventListener('click', () => {
       titleScreenDiv.remove();
       createBoard();
     });
   
-    vsComputerBtn.setAttribute('id', 'vs-computer-btn');
     vsComputerBtn.addEventListener('click', () => {
-      console.log("This feature has not been added yet");
+      alert("This feature has not been added yet");
     });
   }
 
   const createBoard = () => {
-    console.log('hi');
     const boardContainerDiv = document.createElement('div');
+    const btnContainerDiv = document.createElement('div');
     boardContainerDiv.setAttribute('id', 'board-container');
+    btnContainerDiv.setAttribute('id', 'btn-container');
     document.body.append(boardContainerDiv);
+    document.body.append(btnContainerDiv);
+
     for(let i = 0; i < 9; i++){
-      console.log(i);
       const gridDiv = document.createElement('div');
       gridDiv.setAttribute('id', i);
       gridDiv.classList.add('test');
       boardContainerDiv.append(gridDiv);
     }
+
+    const resetBtn = document.createElement('button');
+    resetBtn.setAttribute('id','reset-btn');
+    resetBtn.classList.add('btn');
+    resetBtn.innerHTML = "Reset";
+
+    resetBtn.addEventListener('click', () => {
+      resetBoard();
+    })
+
+    const backBtn = document.createElement('button');
+    backBtn.setAttribute('id', 'back-btn');
+    backBtn.classList.add('btn');
+    backBtn.innerHTML = "Back";
+
+    backBtn.addEventListener('click', () => {
+      backToTitle();
+    })
+
+    btnContainerDiv.append(resetBtn);
+    btnContainerDiv.append(backBtn);
 
     const test = document.querySelectorAll('.test');
     let counter = 0;
@@ -124,31 +149,37 @@ const displayController = (function() {
         }
       });
     });
-    
   }
+
+  const resetBoard = () => {
+    const boardContainerDiv = document.getElementById('board-container');
+    const btnContainerDiv = document.getElementById('btn-container');
+
+    boardContainerDiv.remove();
+    btnContainerDiv.remove();
+
+    createBoard();
+  }
+
+  const backToTitle = () => {
+    const boardContainerDiv = document.getElementById('board-container');
+    const btnContainerDiv = document.getElementById('btn-container');
+
+    boardContainerDiv.remove();
+    btnContainerDiv.remove();
+
+    createTitle();
+  }
+
   return {createTitle}
 })();
 
-const test = document.querySelectorAll('.test');
-let counter = 0;
-test.forEach((item) => {
-  item.addEventListener('click', () => {
-    if (counter % 2 == 0) {
-      if (!item.hasChildNodes()){
-        item.textContent = "O";
-        item.style.color = "rgba(66, 179, 255, 0.5)";
-        gameBoard.playerMove("O", item.id);
-        counter++;
-      }
-    } else if (counter % 2 == 1) {
-      if (!item.hasChildNodes()){
-        item.textContent = "X";
-        item.style.color = "rgb(255, 102, 102)";
-        gameBoard.playerMove("X", item.id);
-        counter++;
-      }
-    }
-  });
-});
+const player = (marker) => {
+
+  return {marker}
+}
+
+const playerX = player('X');
+const playerO = player('O');
 
 displayController.createTitle();
